a65e3bc740f6d915c12d7bf6f63d7d3f89b5429f
Elizabeth Baumel <Icetigris@users.noreply.github.com>
2018-01-04 04:34:04 -0500

    Got a window up.
    Also I forgot to add the SDLmain project to the solution file, which was causing a build error because SDLmain.lib was missing since I never built it. There's still a build issue with SDLmain and include paths, so I just pulled SDL, built myself a .lib, and copied it into Binaries so I could get something other than build system nonsense done.

--- Engine/Source/Main.cpp
+++ Engine/Source/Main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <SDL.h>
 
 void WinMain()
 {
-	std::cout << "Hello world!";
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+	{
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	}
+
+	SDL_Window* SDLWin = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (SDLWin == nullptr) 
+	{
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+	}
+
+	SDL_Renderer *SDLRenderer = SDL_CreateRenderer(SDLWin, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (SDLRenderer == nullptr) 
+	{
+		SDL_DestroyWindow(SDLWin);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+	}
+
+	SDL_RenderClear(SDLRenderer);
+	SDL_RenderPresent(SDLRenderer);
+	SDL_Delay(10000);
+
+	SDL_DestroyRenderer(SDLRenderer);
+	SDL_DestroyWindow(SDLWin);
+	SDL_Quit();
 }
