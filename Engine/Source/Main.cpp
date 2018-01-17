253b924da868b659f6abce8308d454060895ce56
Elizabeth Baumel <elizabeth.baumel@disbelief.com>
2018-01-16 23:00:55 -0500

    Moving more stuff into its own functions.

--- Engine/Source/Main.cpp
+++ Engine/Source/Main.cpp
@@ -44,27 +44,46 @@ void InitSDL(Uint32 InitFlags)
 	}
 }
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+SDL_Window* SDLWin;
+void InitWindow(const char* Title, int PositionX, int PositionY, int Width, int Height, int Flags)
 {
-	// Init SDL subsystems we're going to use.
-	InitSDL(SDL_INIT_VIDEO);
-
-	// Init window
-	SDL_Window* SDLWin = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	SDLWin = SDL_CreateWindow(Title, PositionX, PositionY, Width, Height, Flags);
 	if (SDLWin == nullptr)
 	{
 		MEGALOG("SDL_CreateWindow Error: " << SDL_GetError() << std::endl);
 		SDL_Quit();
 	}
+}
 
-	// Init renderer
-	SDL_Renderer *SDLRenderer = SDL_CreateRenderer(SDLWin, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+SDL_Renderer *SDLRenderer;
+void InitRenderer(SDL_Window* Window)//, GraphicsAPI RequestedAPI)
+{
+	SDLRenderer = SDL_CreateRenderer(SDLWin, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (SDLRenderer == nullptr)
 	{
 		SDL_DestroyWindow(SDLWin);
 		MEGALOG("SDL_CreateRenderer Error: " << SDL_GetError() << std::endl);
 		SDL_Quit();
 	}
+}
+
+void CleanupAndQuit()
+{
+	SDL_DestroyRenderer(SDLRenderer);
+	SDL_DestroyWindow(SDLWin);
+	SDL_Quit();
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// Init SDL subsystems we're going to use.
+	InitSDL(SDL_INIT_VIDEO);
+
+	// Init SDL window
+	InitWindow("Megascops Engine", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+	// Init renderer (default d3d12)
+	InitRenderer(SDLWin);//, RenderAPI)
 
 	// Game loop
 	while (HandleEvents())
@@ -75,7 +94,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		SDL_RenderClear(SDLRenderer);
 		SDL_RenderPresent(SDLRenderer);
 	}
-	SDL_DestroyRenderer(SDLRenderer);
-	SDL_DestroyWindow(SDLWin);
-	SDL_Quit();
+
+	CleanupAndQuit();
 }
