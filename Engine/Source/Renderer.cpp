6ba9e43cb68e381353c6b99f21a42bfa23e9eab7
Icetigris <Icetigris@users.noreply.github.com>
2018-02-09 12:53:43 -0500

    Got a d3d12 window up now. Added d3dx12.h for the helper functions.

--- Engine/Source/Renderer.cpp
+++ Engine/Source/Renderer.cpp
@@ -7,6 +7,7 @@
 
 #include <d3d12.h>
 #include <dxgi1_4.h>
+#include "d3dx12.h"
 
 HWND WindowHandle; //used in swapchain
 
@@ -130,14 +131,14 @@ void CreateRenderer(SDL_Window* SDLWin)
 	swapChainDesc.SampleDesc.Count = 1;
 
 	IDXGISwapChain1* swapChain;
-	(factory->CreateSwapChainForHwnd(
+	factory->CreateSwapChainForHwnd(
 		m_commandQueue,		// Swap chain needs the queue so that it can force a flush on it.
 		WindowHandle,
 		&swapChainDesc,
 		nullptr,
 		nullptr,
 		&swapChain
-	));
+	);
 
 	// This sample does not support fullscreen transitions.
 	(factory->MakeWindowAssociation(WindowHandle, DXGI_MWA_NO_ALT_ENTER));
@@ -159,25 +160,127 @@ void CreateRenderer(SDL_Window* SDLWin)
 	}
 
 	// Create frame resources.
-// 	{
-// 		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
-// 
-// 		// Create a RTV for each frame.
-// 		for (UINT n = 0; n < FrameCount; n++)
-// 		{
-// 			(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])));
-// 			m_device->CreateRenderTargetView(m_renderTargets[n], nullptr, rtvHandle);
-// 			rtvHandle.Offset(1, m_rtvDescriptorSize);
-// 		}
-// 	}
+ 	{
+ 		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+ 
+ 		// Create a RTV for each frame.
+ 		for (UINT n = 0; n < FrameCount; n++)
+ 		{
+ 			m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n]));
+ 			m_device->CreateRenderTargetView(m_renderTargets[n], nullptr, rtvHandle);
+ 			rtvHandle.Offset(1, m_rtvDescriptorSize);
+ 		}
+ 	}
 
 	m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator));
+
+	// Create the command list.
+	m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator, nullptr, IID_PPV_ARGS(&m_commandList));
+
+	// Command lists are created in the recording state, but there is nothing
+	// to record yet. The main loop expects it to be closed, so close it now.
+	m_commandList->Close();
+
+	// Create synchronization objects.
+	{
+		(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+		m_fenceValue = 1;
+
+		// Create an event handle to use for frame synchronization.
+		m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+		if (m_fenceEvent == nullptr)
+		{
+			(HRESULT_FROM_WIN32(GetLastError()));
+		}
+	}
 }
 
 void Render()
 {
+//-------
+	// Command list allocators can only be reset when the associated 
+	// command lists have finished execution on the GPU; apps should use 
+	// fences to determine GPU execution progress.
+	(m_commandAllocator->Reset());
+
+	// However, when ExecuteCommandList() is called on a particular command 
+	// list, that command list can then be reset at any time and must be before 
+	// re-recording.
+	(m_commandList->Reset(m_commandAllocator, m_pipelineState));
+
+	// Indicate that the back buffer will be used as a render target.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex], D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+
+	// Record commands.
+	const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+	m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+	// Indicate that the back buffer will now be used to present.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex], D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	m_commandList->Close();
+//-------
+	// Execute the command list.
+	ID3D12CommandList* ppCommandLists[] = { m_commandList };
+	m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+	// Present the frame.
+	(m_swapChain->Present(1, 0));
+//-------
+// WAITING FOR THE FRAME TO COMPLETE BEFORE CONTINUING IS NOT BEST PRACTICE.
+// This is code implemented as such for simplicity. The D3D12HelloFrameBuffering
+// sample illustrates how to use fences for efficient resource usage and to
+// maximize GPU utilization.
+
+// Signal and increment the fence value.
+	const UINT64 fence = m_fenceValue;
+	(m_commandQueue->Signal(m_fence, fence));
+	m_fenceValue++;
+
+	// Wait until the previous frame is finished.
+	if (m_fence->GetCompletedValue() < fence)
+	{
+		(m_fence->SetEventOnCompletion(fence, m_fenceEvent));
+		WaitForSingleObject(m_fenceEvent, INFINITE);
+	}
+
+	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
 }
 
 void DestroyRenderer()
 {
+	// WAITING FOR THE FRAME TO COMPLETE BEFORE CONTINUING IS NOT BEST PRACTICE.
+	// This is code implemented as such for simplicity. The D3D12HelloFrameBuffering
+	// sample illustrates how to use fences for efficient resource usage and to
+	// maximize GPU utilization.
+
+	// Signal and increment the fence value.
+	const UINT64 fence = m_fenceValue;
+	(m_commandQueue->Signal(m_fence, fence));
+	m_fenceValue++;
+
+	// Wait until the previous frame is finished.
+	if (m_fence->GetCompletedValue() < fence)
+	{
+		(m_fence->SetEventOnCompletion(fence, m_fenceEvent));
+		WaitForSingleObject(m_fenceEvent, INFINITE);
+	}
+
+	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+	m_commandList->Close();
+
+
+	free(m_fence);
+	free(m_commandList);
+	free(m_commandAllocator);
+	free(m_commandQueue);
+	free(m_rtvHeap);
+	free(m_pipelineState);
+	free(m_renderTargets[0]);
+	free(m_renderTargets[1]);
+	free(m_swapChain);
+	free(m_device);
 }
